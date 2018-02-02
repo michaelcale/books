0db4f9fd660a9b9ff14eaf4b5de4f93c3c98f25f
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-02-02 09:30:34 -0800

    tweak comments

--- s/rename.ps1
+++ s/rename.ps1
@@ -3,10 +3,12 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
 
-# chapters and articles are order by their number prefix (0030-* etc.)
-# we space them by 10 so that we can squeeze an article or 2 when
-# we move articles/chapters around or when we add a new one
-# this script re-balances the numbers to be evenly spaced by 10 again
+# Chapters and articles are order by their number prefix (0030-* etc.).
+# We space them by 10 so that we can squeeze an article or 2 when
+# we move articles/chapters around or when we add a new one.
+# This script re-balances the numbers (using git mv) to be evenly
+# spaced by 10 again.
+# If they are already evenly spaced, it does nothing.
 
 Remove-Item -Force -ErrorAction SilentlyContinue ./cmd/rename/rename
 
