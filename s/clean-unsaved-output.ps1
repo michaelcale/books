817074bef02bbba5f27c8c7884f68986cc918c4e
Krzysztof <kkowalczyk@gmail.com>
2018-03-07 00:38:10 -0800

    fix s/clean-unsaved-output.ps1 for windows

--- s/clean-unsaved-output.ps1
+++ s/clean-unsaved-output.ps1
@@ -3,9 +3,10 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
 
+Remove-Item "cached_output\*" | Where-Object { ! $_.PSIsContainer }
+
 Set-Location -Path cached_output
 
-rm -rf *
 git checkout ./
 
 Set-Location -Path ..
