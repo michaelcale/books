3979246446e7d305af900d06e36ab65c36a402ea
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-03-12 18:00:00 -0700

    make gen-books.ps1 windows friendly

--- s/gen-books.ps1
+++ s/gen-books.ps1
@@ -3,12 +3,12 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 function exitIfFailed { if ($LASTEXITCODE -ne 0) { exit } }
 
-Remove-Item -Force -ErrorAction SilentlyContinue ./cmd/gen-books/gen-books
+Remove-Item -Force -ErrorAction SilentlyContinue ./cmd/gen-books/gen-books.exe
 
-Set-Location -Path cmd/gen-books
-go build -o gen-books
+Set-Location -Path cmd/gen-books.exe
+go build -o gen-books.exe
 Set-Location -Path ../..
 exitIfFailed
 
-./cmd/gen-books/gen-books $args
-Remove-Item -Force -ErrorAction SilentlyContinue ./cmd/gen-books/gen-books
+./cmd/gen-books/gen-books.exe $args
+Remove-Item -Force -ErrorAction SilentlyContinue ./cmd/gen-books/gen-books.exe
